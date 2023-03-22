0a640ab356cb86ca11cca87293c5738786bceeef
DELL <DELL@DESKTOP-KLJI07S>
2023-03-22 20:06:31 +0700

    proses prosedur untuk input

--- bubblesort_170/bubblesort_170.cpp
+++ bubblesort_170/bubblesort_170.cpp
@@ -8,4 +8,7 @@ int n;				//Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 void input() {		//Procedure untuk input 
 	while (true) {	//Looping 
-		cout << "Masukan banyaknya elemen pada "
+		cout << "Masukan banyaknya elemen pada array : "; //Output ke layar
+		cin << n;	//Input dari pengguna 
+		if (n <= 20) //Jika n kurang dari atau sama dengan 20
+			break;	 //Keluar dari loop
